40b93cad8f809caa91e9b67cceb1fdef70de6852
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-01-10 17:13:44 +0300

    [starts#163074995] adds requiremets.txt

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify, request, make_response
+
+import os
+
+from app import create_app
+
+config_name = os.getenv('APP_ENV')
+app = create_app(config_name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+
+    
