337d6a73a29041fe138638eb1a74b9c680545b1d
MbuguaCaleb <mbuguacaleb30@gmail.com>
2019-01-16 11:35:35 +0300

    [finishes#163248757] by adding configuration details

--- app/__init__.py
+++ app/__init__.py
@@ -5,24 +5,24 @@ from flask_jwt_extended import (JWTManager)
 from app.api.v1.views.meetup_view import v1 as meetups_blueprint_v1
 from app.api.v1.views.questions_views import v1 as questions_blueprint_v1
 
-def create_app(config_name="development"):
+def create_app(config_name):
     """ Function to initialize Flask app """
 
     config_name = os.environ.get('FLASK_CONFIG', 'development')
 
     # Initialize app
     app = Flask(__name__, instance_relative_config=True)
-
     app.config.from_object(app_config[config_name])
-
     app.config.from_pyfile('config.py')
 
  
     # Register V1 Blueprints
-   
     app.register_blueprint(meetups_blueprint_v1)
     app.register_blueprint(questions_blueprint_v1)
     
 
     return app
 
+
+
+    
